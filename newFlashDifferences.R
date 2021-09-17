bd1412524072356373063ecf69910f9b5ce7a73c
disandroa <adisandro818@gmail.com>
2021-09-17 13:24:41 -0400

    added loop for t-tests

--- newFlashDifferences.R
+++ newFlashDifferences.R
@@ -13,6 +13,7 @@ library(arules)
 library(stringr)
 library(visreg)
 library(lubridate)
+library(mgcv)
 
 
 # Load and organize data ----
@@ -36,59 +37,304 @@ bigcnb <- bigcnb[order(bigcnb$Datasetid),]
 
 
 # * Separate into test versions ----
-tests <- sort(unique(bigcnb$Version))
+ADT36_A <- bigcnb[bigcnb$Version == "ADT36_A" & !is.na(bigcnb$Version) & !is.na(bigcnb$Accuracy),]
+ADT60_A <- bigcnb[bigcnb$Version == "ADT60_A" & !is.na(bigcnb$Version) & !is.na(bigcnb$Accuracy),]
 
-ADT36_A <- bigcnb[bigcnb$Version == "ADT36_A" & !is.na(bigcnb$Version),]
-ADT60_A <- bigcnb[bigcnb$Version == "ADT60_A" & !is.na(bigcnb$Version),]
+AIM <- bigcnb[bigcnb$Version == "AIM" & !is.na(bigcnb$Version) & !is.na(bigcnb$Accuracy),]
 
-CPF_A <- bigcnb[bigcnb$Version == "CPF_A" & !is.na(bigcnb$Version),]
-CPF_B <- bigcnb[bigcnb$Version == "CPF_B" & !is.na(bigcnb$Version),]
-CPFD_A <- bigcnb[bigcnb$Version == "CPFD_A" & !is.na(bigcnb$Version),]
-CPFD_B <- bigcnb[bigcnb$Version == "CPFD_B" & !is.na(bigcnb$Version),]
+CPF_A <- bigcnb[bigcnb$Version == "CPF_A" & !is.na(bigcnb$Version) & !is.na(bigcnb$Accuracy),]
+CPF_B <- bigcnb[bigcnb$Version == "CPF_B" & !is.na(bigcnb$Version) & !is.na(bigcnb$Accuracy),]
+CPFD_A <- bigcnb[bigcnb$Version == "CPFD_A" & !is.na(bigcnb$Version) & !is.na(bigcnb$Accuracy),]
+CPFD_B <- bigcnb[bigcnb$Version == "CPFD_B" & !is.na(bigcnb$Version) & !is.na(bigcnb$Accuracy),]
 
-ER40_A <- bigcnb[bigcnb$Version == "ER40_A" & !is.na(bigcnb$Version),]
-ER40_C <- bigcnb[bigcnb$Version == "ER40_C" & !is.na(bigcnb$Version),]
-ER40_D <- bigcnb[bigcnb$Version == "ER40_D" & !is.na(bigcnb$Version),]
+ER40_A <- bigcnb[bigcnb$Version == "ER40_A" & !is.na(bigcnb$Version) & !is.na(bigcnb$Accuracy),]
+ER40_C <- bigcnb[bigcnb$Version == "ER40_C" & !is.na(bigcnb$Version) & !is.na(bigcnb$Accuracy),]
+ER40_D <- bigcnb[bigcnb$Version == "ER40_D" & !is.na(bigcnb$Version) & !is.na(bigcnb$Accuracy),]
 
-GNG150 <- bigcnb[bigcnb$Version == "GNG150" & !is.na(bigcnb$Version),]
+GNG150 <- bigcnb[bigcnb$Version == "GNG150" & !is.na(bigcnb$Version) & !is.na(bigcnb$Accuracy),]
 
-KCPW_A <- bigcnb[bigcnb$Version == "KCPW_A" & !is.na(bigcnb$Version),]
-KCPWD_A <- bigcnb[bigcnb$Version == "KCPWD_A" & !is.na(bigcnb$Version),]
+KCPW_A <- bigcnb[bigcnb$Version == "KCPW_A" & !is.na(bigcnb$Version) & !is.na(bigcnb$Accuracy),]
+KCPWD_A <- bigcnb[bigcnb$Version == "KCPWD_A" & !is.na(bigcnb$Version) & !is.na(bigcnb$Accuracy),]
 
-KSPVRT_A <- bigcnb[bigcnb$Version == "KSPVRT_A" & !is.na(bigcnb$Version),]
-KSPVRT_B <- bigcnb[bigcnb$Version == "KSPVRT_B" & !is.na(bigcnb$Version),]
-KSPVRT_D <- bigcnb[bigcnb$Version == "KSPVRT_D" & !is.na(bigcnb$Version),]
+KSPVRT_A <- bigcnb[bigcnb$Version == "KSPVRT_A" & !is.na(bigcnb$Version) & !is.na(bigcnb$Accuracy),]
+KSPVRT_B <- bigcnb[bigcnb$Version == "KSPVRT_B" & !is.na(bigcnb$Version) & !is.na(bigcnb$Accuracy),]
+KSPVRT_D <- bigcnb[bigcnb$Version == "KSPVRT_D" & !is.na(bigcnb$Version) & !is.na(bigcnb$Accuracy),]
 
-MEDF36_A <- bigcnb[bigcnb$Version == "MEDF36_A" & !is.na(bigcnb$Version),]
-MEDF60_A <- bigcnb[bigcnb$Version == "MEDF60_A" & !is.na(bigcnb$Version),]
+MEDF36_A <- bigcnb[bigcnb$Version == "MEDF36_A" & !is.na(bigcnb$Version) & !is.na(bigcnb$Accuracy),]
+MEDF60_A <- bigcnb[bigcnb$Version == "MEDF60_A" & !is.na(bigcnb$Version) & !is.na(bigcnb$Accuracy),]
 
-PCET_A <- bigcnb[bigcnb$Version == "PCET_A" & !is.na(bigcnb$Version),]
-SPCET_A <- bigcnb[bigcnb$Version == "SPCET_A" & !is.na(bigcnb$Version),]
+MPRACT <- bigcnb[bigcnb$Version == "MPRACT" & !is.na(bigcnb$Version) & !is.na(bigcnb$Accuracy),]
 
-PMAT18_B <- bigcnb[bigcnb$Version == "PMAT18_B" & !is.na(bigcnb$Version),]
-PMAT24_A <- bigcnb[bigcnb$Version == "PMAT24_A" & !is.na(bigcnb$Version),]
-PMAT24_B <- bigcnb[bigcnb$Version == "PMAT24_B" & !is.na(bigcnb$Version),]
+PCET_A <- bigcnb[bigcnb$Version == "PCET_A" & !is.na(bigcnb$Version) & !is.na(bigcnb$Accuracy),]
+SPCET_A <- bigcnb[bigcnb$Version == "SPCET_A" & !is.na(bigcnb$Version) & !is.na(bigcnb$Accuracy),]
 
-SLNB2_90 <- bigcnb[bigcnb$Version == "SLNB2_90" & !is.na(bigcnb$Version),]
+PMAT18_B <- bigcnb[bigcnb$Version == "PMAT18_B" & !is.na(bigcnb$Version) & !is.na(bigcnb$Accuracy),]
+PMAT24_A <- bigcnb[bigcnb$Version == "PMAT24_A" & !is.na(bigcnb$Version) & !is.na(bigcnb$Accuracy),]
+PMAT24_B <- bigcnb[bigcnb$Version == "PMAT24_B" & !is.na(bigcnb$Version) & !is.na(bigcnb$Accuracy),]
 
-SPLOT12 <- bigcnb[bigcnb$Version == "SPLOT12" & !is.na(bigcnb$Version),]
-VSPLOT15 <- bigcnb[bigcnb$Version == "VSPLOT15" & !is.na(bigcnb$Version),]
-VSPLOT24 <- bigcnb[bigcnb$Version == "VSPLOT24" & !is.na(bigcnb$Version),]
+SCTAP <- bigcnb[bigcnb$Version == "SCTAP" & !is.na(bigcnb$Version) & !is.na(bigcnb$Accuracy),]
 
-SPVRT_A <- bigcnb[bigcnb$Version == "SPVRT_A" & !is.na(bigcnb$Version),]
+SLNB2_90 <- bigcnb[bigcnb$Version == "SLNB2_90" & !is.na(bigcnb$Version) & !is.na(bigcnb$Accuracy),]
 
-SVOLT_A <- bigcnb[bigcnb$Version == "SVOLT_A" & !is.na(bigcnb$Version),]
-SVOLTD_A <- bigcnb[bigcnb$Version == "SVOLTD_A" & !is.na(bigcnb$Version),]
+SPLOT12 <- bigcnb[bigcnb$Version == "SPLOT12" & !is.na(bigcnb$Version) & !is.na(bigcnb$Accuracy),]
+VSPLOT15 <- bigcnb[bigcnb$Version == "VSPLOT15" & !is.na(bigcnb$Version) & !is.na(bigcnb$Accuracy),]
+VSPLOT24 <- bigcnb[bigcnb$Version == "VSPLOT24" & !is.na(bigcnb$Version) & !is.na(bigcnb$Accuracy),]
 
+SPVRT_A <- bigcnb[bigcnb$Version == "SPVRT_A" & !is.na(bigcnb$Version) & !is.na(bigcnb$Accuracy),]
 
+SVOLT_A <- bigcnb[bigcnb$Version == "SVOLT_A" & !is.na(bigcnb$Version) & !is.na(bigcnb$Accuracy),]
+SVOLTD_A <- bigcnb[bigcnb$Version == "SVOLTD_A" & !is.na(bigcnb$Version) & !is.na(bigcnb$Accuracy),]
+
+texts <- sort(unique(bigcnb$Version))
+'%!in%' <- function(x,y)!('%in%'(x,y))
+notthese <- c("ADT60_A","CPFD_A","CPFD_B","ER40_A","ER40_C","KCPWD_A","KSPVRT_A","KSPVRT_B",
+              "MEDF60_A","MPRACT","PMAT18_B","PMAT24_B","SCTAP","SPCET_A","SPLOT12","SPVRT_A",
+              "SVOLTD_A","VSPLOT24")
+texts <- texts[texts %!in% notthese] # getting rid of the tests that only have flash, no non-flash subjects after correcting for the existence of age and sex
+tests <- mget(texts)
 
 # Models and Plotting ----
+
+# 9.16.21 implementing what I talked about with Kosha and Tyler
+
+# * two-factor FA, waiting itemwise data for this ----
+
+
+# * t-tests ----
+
+fit <- lm(Accuracy ~ age + Gender + I(age^2) + I(age^3), data = ADT36_A)
+# summary(fit)
+visreg(fit,"age", by="Gender")
+
+res <- scale(resid(fit))   # scaled residuals
+newADT36 <- ADT36_A[!is.na(ADT36_A$Accuracy) & !is.na(ADT36_A$age) & !is.na(ADT36_A$Gender),]
+flash <- newADT36[newADT36$flash==1,]
+nflash <- newADT36[newADT36$flash==0,]
+# nrow(flash)
+# nrow(nflash)
+frownames <- row.names(flash)
+nfrownames <- row.names(nflash)
+
+res <- as.data.frame(res)
+names(res) <- "residuals"
+res$flash <- 1
+for (i in 1:nrow(res)) {
+  if (row.names(res[i,]) %in% nfrownames) {
+    res[i,]$flash <- 0
+  } else{}
+}
+
+ttest <- t.test(res$residuals~res$flash)
+
+
+# running the same thing with only the last year of flash
+cutoff <- as.Date("2019-12-31")
+lastyear <- ADT36_A[ADT36_A$Dotest >= cutoff,]
+fit <- lm(Accuracy ~ age + Gender + I(age^2) + I(age^3), data = lastyear)
+# summary(fit)
+visreg(fit,"age", by="Gender")
+
+res <- scale(resid(fit))   # scaled residuals
+newADT36 <- lastyear[!is.na(lastyear$Accuracy) & !is.na(lastyear$age) & !is.na(lastyear$Gender),]
+flash <- newADT36[newADT36$flash==1,]
+nflash <- newADT36[newADT36$flash==0,]
+# nrow(flash)
+# nrow(nflash)
+frownames <- row.names(flash)
+nfrownames <- row.names(nflash)
+
+res <- as.data.frame(res)
+names(res) <- "residuals"
+res$flash <- 1
+for (i in 1:nrow(res)) {
+  if (row.names(res[i,]) %in% nfrownames) {
+    res[i,]$flash <- 0
+  } else{}
+}
+
+ttest <- t.test(res$residuals~res$flash)
+
+# same t-tests but with gam instead of lm
+fit <- gam(Accuracy ~ s(age) + Gender + s(I(age^2)) + s(I(age^3)), data = ADT36_A)
+# summary(fit)
+visreg(fit,"age", by="Gender")
+
+res <- scale(resid(fit))   # scaled residuals
+newADT36 <- ADT36_A[!is.na(ADT36_A$Accuracy) & !is.na(ADT36_A$age) & !is.na(ADT36_A$Gender),]
+flash <- newADT36[newADT36$flash==1,]
+nflash <- newADT36[newADT36$flash==0,]
+# nrow(flash)
+# nrow(nflash)
+frownames <- row.names(flash)
+nfrownames <- row.names(nflash)
+
+res <- as.data.frame(res)
+rownames(res) <- rownames(newADT36)
+names(res) <- "residuals"
+res$flash <- 1
+for (i in 1:nrow(res)) {
+  if (row.names(res[i,]) %in% nfrownames) {
+    res[i,]$flash <- 0
+  } else{}
+}
+
+ttest <- t.test(res$residuals~res$flash)
+
+
+# running the same thing with only the last year of flash
+cutoff <- as.Date("2019-12-31")
+lastyear <- ADT36_A[ADT36_A$Dotest >= cutoff,]
+fit <- gam(Accuracy ~ s(age) + Gender + s(I(age^2)) + s(I(age^3)), data = lastyear)
+# summary(fit)
+visreg(fit,"age", by="Gender")
+
+res <- scale(resid(fit))   # scaled residuals
+newADT36 <- lastyear[!is.na(lastyear$Accuracy) & !is.na(lastyear$age) & !is.na(lastyear$Gender),]
+flash <- newADT36[newADT36$flash==1,]
+nflash <- newADT36[newADT36$flash==0,]
+# nrow(flash)
+# nrow(nflash)
+frownames <- row.names(flash)
+nfrownames <- row.names(nflash)
+
+res <- as.data.frame(res)
+rownames(res) <- rownames(newADT36)
+names(res) <- "residuals"
+res$flash <- 1
+for (i in 1:nrow(res)) {
+  if (row.names(res[i,]) %in% nfrownames) {
+    res[i,]$flash <- 0
+  } else{}
+}
+
+ttest <- t.test(res$residuals~res$flash)
+
+
+# only age in s()
+fit <- gam(Accuracy ~ s(age) + Gender + I(age^2) + I(age^3), data = lastyear)
+# summary(fit)
+visreg(fit,"age", by="Gender")
+
+res <- scale(resid(fit))   # scaled residuals
+newADT36 <- lastyear[!is.na(lastyear$Accuracy) & !is.na(lastyear$age) & !is.na(lastyear$Gender),]
+flash <- newADT36[newADT36$flash==1,]
+nflash <- newADT36[newADT36$flash==0,]
+# nrow(flash)
+# nrow(nflash)
+frownames <- row.names(flash)
+nfrownames <- row.names(nflash)
+
+res <- as.data.frame(res)
+rownames(res) <- rownames(newADT36)
+names(res) <- "residuals"
+res$flash <- 1
+for (i in 1:nrow(res)) {
+  if (row.names(res[i,]) %in% nfrownames) {
+    res[i,]$flash <- 0
+  } else{}
+}
+
+ttest <- t.test(res$residuals~res$flash)
+
+
+
+# general code for t-tests
+for (i in 1:length(texts)) {
+  test <- tests[[i]]
+  
+  # alldates
+  fit <- gam(Accuracy ~ s(age) + Gender + s(I(age^2)) + s(I(age^3)), data = test)
+  
+  # save summary of this model as a variable
+  name <- paste0(texts[i],"fitAD")
+  assign(name,summary(fit))
+  
+  # is this plot necesssary?
+  visreg(fit,"age", by="Gender")
+  
+  res <- scale(resid(fit))   # scaled residuals
+  newtest <- test[!is.na(test$Accuracy) & !is.na(test$age) & !is.na(test$Gender),]
+  nflash <- newtest[newtest$flash==0,]
+  nfrownames <- row.names(nflash)
+  
+  res <- as.data.frame(res)
+  rownames(res) <- rownames(newtest)
+  names(res) <- "residuals"
+  res$flash <- 1
+  for (j in 1:nrow(res)) {
+    if (row.names(res[j,]) %in% nfrownames) {
+      res[j,]$flash <- 0
+    } else{}
+  }
+  
+  ttest <- t.test(res$residuals~res$flash)
+  # save ttest as a variable
+  name <- paste0(texts[i],"ttestAD")
+  assign(name,ttest)
+  
+  
+  # lastyear
+  cutoff <- as.Date("2019-12-31")
+  lastyear <- test[test$Dotest >= cutoff,]
+  fit <- gam(Accuracy ~ s(age) + Gender + s(I(age^2)) + s(I(age^3)), data = lastyear)
+  
+  # save summary of this model as a variable
+  name <- paste0(texts[i],"fitLY")
+  assign(name,summary(fit))
+  
+  visreg(fit,"age", by="Gender")
+  
+  res <- scale(resid(fit))   # scaled residuals
+  newtest <- lastyear[!is.na(lastyear$Accuracy) & !is.na(lastyear$age) & !is.na(lastyear$Gender),]
+  nflash <- newtest[newtest$flash==0,]
+  nfrownames <- row.names(nflash)
+  
+  res <- as.data.frame(res)
+  rownames(res) <- rownames(newtest)
+  names(res) <- "residuals"
+  res$flash <- 1
+  for (j in 1:nrow(res)) {
+    if (row.names(res[j,]) %in% nfrownames) {
+      res[j,]$flash <- 0
+    } else{}
+  }
+  
+  ttest <- t.test(res$residuals~res$flash)
+  # save ttest as a variable
+  name <- paste0(texts[i],"ttestLY")
+  assign(name,ttest)
+}
+
+
+
+
+
+# * correlations ----
+
+
+
+
+
+
+
+# * flash/non-flash intra-subject correlations ----
+
+
+
+
+
+
+
+
+
+
+
 
 # old script only used siteid, bblid, age, dotest, gender, flash, CR, RTCR as the important columns
 # i can do that again here, or I can just call on the columns i need, leaving the 
 # unneeded ones in still.
 
-# * CPF_A, 40 total ----
+# * CPF_A (40 total) ----
 
 CPFAnoage <- CPF_A[which(is.na(CPF_A$age)),] # there are some missing ages
 CPF_A <- CPF_A[!is.na(CPF_A$Accuracy),]
@@ -327,26 +573,6 @@ dev.off()
 
 
 # test ----
-agesexfit <- lm(Accuracy ~ flash + age + Gender, data=CPF_A)
-summary(agesexfit)
-v1 <- visreg(agesexfit, "flash", by= "age", cond = list(Gender="M"), breaks = 5, overlay =T, gg=T) +
-  theme_bw() +
-  theme(plot.margin=unit(c(1,2,1.5,1.2),"cm")) +
-  labs(y = "Score (out of 40)", 
-       title = "Male Accuracy on CPF_A by age") +
-  scale_x_continuous(name = "Flash", breaks = seq(0,1,by=1))
-v2 <- visreg(agesexfit, "flash", by= "age", cond = list(Gender="F"), breaks = 5, overlay =T, gg=T) +
-  theme_bw() +
-  theme(plot.margin=unit(c(1,2,1.5,1.2),"cm")) +
-  labs(y = "Score (out of 40)",
-       title = "Female Accuracy on CPF_A by age") +
-  scale_x_continuous(name = "Flash", breaks = seq(0,1,by=1))
-
-# trying to put the above two plots in the same display
-v <- visregList(v1,
-                v2,
-                labels=c("Male", "Female"), collapse=TRUE)
-plot(v, ylab="Score (out of 40)")
 
 
 
