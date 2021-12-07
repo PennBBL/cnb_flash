817058e03270b3dfba25380e6b53c9636003ec54
disandroa <adisandro818@gmail.com>
2021-12-07 13:35:13 -0500

    got rid of more bad data

--- FA_itemwise.R
+++ FA_itemwise.R
@@ -46,6 +46,9 @@ noNA_N <- pcptn360[rowSums(is.na(pcptn360)) <5,]     # pcpt[n]360   flash
 noNA_sNL <- spcptnl[rowSums(is.na(spcptnl)) <5,]     # spcpt[nl]    both
 noNA_sN <- spcptn90[rowSums(is.na(spcptn90)) <5,]    # spcpt[n]90   both
 
+sNL_acc <- noNA_sNL
+sN_acc <- noNA_sN
+
 # only need sNL and sN since these tests were administered both flash and non-flash
 sNL_f <- noNA_sNL[noNA_sNL$flash ==1,grepl("CORR",colnames(noNA_sNL))]  # noNA_sNL flash group
 sNL_n <- noNA_sNL[noNA_sNL$flash ==0,grepl("CORR",colnames(noNA_sNL))]  # noNA_sNL non-flash group
@@ -223,7 +226,7 @@ pro_sum_sNcorr1 <- cor(pro_sum_sN[,c(2,4)])     # 0.815
 pro_sum_sNcorr2 <- cor(pro_sum_sN[,c(3,5)])     # 0.841
 
 
-# summary table
+# summary table ----
 summary <- data.frame(matrix(NA,nrow=4,ncol = 5))
 names(summary) <- c("Exp_oblimin","Exp_promax","MIRT_oblimin","MIRT_promax","Alpha")
 rownames(summary) <- c("SPCPTNL_flash","SPCPTNL_nonflash","SPCPTN90_flash","SPCPTN90_nonflash")
@@ -241,7 +244,7 @@ write.csv(summary,"SPCPT_flashdif_interfactorcorr.csv")
 
 
 
-# speed differences
+# speed differences ----
 pcptnl <- cpt_iw[,grepl("PCPTNL.CPT_Q", colnames(cpt_iw))]
 pcptn360 <- cpt_iw[,grepl("PCPTN360.CPT_Q", colnames(cpt_iw))]
 spcptnl <- cpt_iw[,grepl("SPCPTNL.SCPT_Q", colnames(cpt_iw))]
@@ -263,21 +266,105 @@ noNA_N <- pcptn360[rowSums(!is.na(pcptn360)) > 5,]     # pcpt[n]360   flash
 noNA_sNL <- spcptnl[rowSums(!is.na(spcptnl)) > 5,]     # spcpt[nl]    both
 noNA_sN <- spcptn90[rowSums(!is.na(spcptn90)) > 5,]    # spcpt[n]90   both
 
-noNA_sNL <- subset(noNA_sNL,  (rowSums(sign(noNA_sNL[,5:184]) < 0,na.rm=T) == 0))
-noNA_sN <- subset(noNA_sN,  (rowSums(sign(noNA_sN[,5:94]) < 0,na.rm=T) == 0))
+colnames(noNA_sNL)[5:184] <- paste0("item_",1:180)
+colnames(noNA_sN)[5:94] <- paste0("item_",1:90)
+
+
+# making sure that there is a TTR for all CORR == 1
+sNL_speed <- noNA_sNL
+sN_speed <- noNA_sN
+both <- intersect(unique(sNL_speed$test_sessions.datasetid),unique(sNL_acc$test_sessions.datasetid))
+sNL_speed <- sNL_speed[sNL_speed$test_sessions.datasetid %in% both,]   # doesn't change anything because all of the TTR data exists in corr too but not in CORR
+sNL_acc <- sNL_acc[sNL_acc$test_sessions.datasetid %in% both,]
+both <- intersect(unique(sN_speed$test_sessions.datasetid),unique(sN_acc$test_sessions.datasetid))
+sN_speed <- sN_speed[sN_speed$test_sessions.datasetid %in% both,]
+sN_acc <- sN_acc[sN_acc$test_sessions.datasetid %in% both,]
+
+sNL_speed <- sNL_speed[match(sNL_speed$test_sessions.datasetid,sNL_acc$test_sessions.datasetid),5:184]
+sN_speed <- sN_speed[match(sN_speed$test_sessions.datasetid,sN_acc$test_sessions.datasetid),5:94]
+sNL_acc <- sNL_acc[5:94]
+sN_acc <- sN_acc[5:94]
+
+sN_corr <- sN_speed
+sN_corr[!is.na(sN_corr)] <- NA
+for (i in 1:nrow(sN_speed)){
+        spe <- sN_speed[i,]
+        acc <- sN_acc[i,]
+        sN_corr[i,] <- ifelse(acc==1, ifelse(!is.na(spe),1,0), ifelse(is.na(spe),1,NA))
+}
+sNL_corr <- sNL_speed
+sNL_corr[!is.na(sNL_corr)] <- NA
+for (i in 1:nrow(sNL_speed)){
+        spe <- sNL_speed[i,]
+        acc <- sNL_acc[i,]
+        sNL_corr[i,] <- ifelse(acc==1, ifelse(!is.na(spe),1,0), ifelse(is.na(spe),1,NA))
+}
+
+
+# not done with the part above, but I ended up finding that there are groups of items that everyone has the correct TTR and CORR relationship but only in weird intervals. (when corr=1, ttr!=NA, and corr=0, ttr!=NA)
+
+
+negative <- subset(noNA_sNL, (rowSums(sign(noNA_sNL[,5:184]) < 1,na.rm=T) > 0))
+toolong <- subset(noNA_sNL, rowSums(noNA_sNL[,5:184] > 1000, na.rm=T) > 0)
+
+write.csv(negative,"SPCPTNL_negative.csv",row.names = F)
+write.csv(toolong,"SPCPTNL_toolong.csv",row.names = F)
+
+noNA_sNL <- subset(noNA_sNL, (rowSums(sign(noNA_sNL[,5:184]) <= 0,na.rm=T) == 0))
+noNA_sN <- subset(noNA_sN, (rowSums(sign(noNA_sN[,5:94]) <= 0,na.rm=T) == 0))
+
+temp <- noNA_sNL[,5:184]
+temp[temp >= 1200 | 200 > temp] <- NA
+noNA_sNL[,5:184] <- temp
+temp <- noNA_sN[,5:94]
+temp[temp >= 1200 | 200 > temp] <- NA
+noNA_sN[,5:94] <- temp
+
+colsums_sNL <- colSums(!is.na(noNA_sNL[,5:184]),na.rm = T)
+colsums_sN <- colSums(!is.na(noNA_sN[,5:94]),na.rm = T)
+
+min(colsums_sNL)   # no items with less than 20 respondents
+min(colsums_sN)    
+
+noNA_sN <- cbind(noNA_sN[,1:4],noNA_sN[,c(names(colsums_sN[colsums_sN>20]))])
+
 
 # only need sNL and sN since these tests were administered both flash and non-flash
-sNL_fs <- noNA_sNL[noNA_sNL$flash ==1,grepl("TTR",colnames(noNA_sNL))]  # noNA_sNL flash group
-sNL_ns <- noNA_sNL[noNA_sNL$flash ==0,grepl("TTR",colnames(noNA_sNL))]  # noNA_sNL non-flash group
-sN_fs <- noNA_sN[noNA_sN$flash ==1,grepl("TTR",colnames(noNA_sN))]  # noNA_sN flash group
-sN_ns <- noNA_sN[noNA_sN$flash ==0,grepl("TTR",colnames(noNA_sN))]  # noNA_sN non-flash group
+sNL_fs <- noNA_sNL[noNA_sNL$flash ==1,grepl("item",colnames(noNA_sNL))]  # noNA_sNL flash group
+sNL_ns <- noNA_sNL[noNA_sNL$flash ==0,grepl("item",colnames(noNA_sNL))]  # noNA_sNL non-flash group
+sN_fs <- noNA_sN[noNA_sN$flash ==1,grepl("item",colnames(noNA_sN))]  # noNA_sN flash group
+sN_ns <- noNA_sN[noNA_sN$flash ==0,grepl("item",colnames(noNA_sN))]  # noNA_sN non-flash group
 
-# not sure how to get rid of outliers for this
 
+sNL_fmeans <- data.frame(colMeans(sNL_fs,na.rm = T))
+sNL_nmeans <- data.frame(colMeans(sNL_ns,na.rm = T))
+sN_fmeans <- data.frame(colMeans(sN_fs,na.rm = T))
+sN_nmeans <- data.frame(colMeans(sN_ns,na.rm = T))
+
+sNL_fsd <- data.frame(apply(sNL_fs, 2, sd,na.rm=T))
+sNL_nsd <- data.frame(apply(sNL_ns, 2, sd,na.rm=T))
+sN_fsd <- data.frame(apply(sN_fs, 2, sd,na.rm=T))
+sN_nsd <- data.frame(apply(sN_ns, 2, sd,na.rm=T))
+
+sNL_fmeansd <- cbind(rownames_to_column(sNL_fmeans),sNL_fsd)
+names(sNL_fmeansd) <- c("Item","MeanTTR","sd")
+sNL_fmeansd$Flash <- 1
+sNL_nmeansd <- cbind(rownames_to_column(sNL_nmeans),sNL_nsd)
+names(sNL_nmeansd) <- c("Item","MeanTTR","sd")
+sNL_nmeansd$Flash <- 0
+sN_fmeansd <- cbind(rownames_to_column(sN_fmeans),sN_fsd)
+names(sN_fmeansd) <- c("Item","MeanTTR","sd")
+sN_fmeansd$Flash <- 1
+sN_nmeansd <- cbind(rownames_to_column(sN_nmeans),sN_nsd)
+names(sN_nmeansd) <- c("Item","MeanTTR","sd")
+sN_nmeansd$Flash <- 0
+
+sNL_meansd <- rbind(sNL_fmeansd,sNL_nmeansd)
+sN_meansd <- rbind(sN_fmeansd,sN_nmeansd)
 
 # need to convert to long format first
-sNL <- gather(noNA_sNL,"item", "ttr", SPCPTNL.SCPT_QID000001_0_TTR:SPCPTNL.SCPT_QID000036_4_TTR, factor_key=TRUE)
-sN <- gather(noNA_sN, "item", "ttr", SPCPTN90.SCPT_QID000001_0_TTR:SPCPTN90.SCPT_QID000018_4_TTR, factor_key=TRUE)
+sNL <- gather(noNA_sNL,"item", "ttr", item_1:item_180, factor_key=TRUE)
+sN <- gather(noNA_sN, "item", "ttr", item_1:item_90, factor_key=TRUE)
 
 # trying to change x-axis labels to be easier to read
 sNLnames <- names(noNA_sNL[,5:184])
@@ -290,22 +377,141 @@ sNLnames <- data.frame(sNLnames)
 # graphs
 spe_box <- ggplot(sNL,aes(x=item,y=ttr,color=factor(flash))) +
         geom_boxplot() +
-        theme_bw() + xlab("Item") +
-        scale_color_discrete(name = "Test Administration", labels = c("Non-Flash", "Flash")) +
-        # scale_x_discrete(name="Item",breaks=colnames(noNA_sNL[,5:184]),limits=factor(sNLnames)) +
-        labs(title = paste("Flash differences in SPCPTNL Speed")) +
-        theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+        theme_bw() + xlab("Item") + ylab("Response Time (ms)") +
+        scale_color_discrete(name = "Test Administration", labels = c("Non-Flash (n=1511)", "Flash (n=20475)")) +
+        labs(title = paste("Flash differences in SPCPTNL Speed"))
 
-ggsave("SPCPTNL_box_flash.png")
-
+ggsave("SPCPTNL_box_flash1.png",height = 10,width = 30)
 
 spe2_box <- ggplot(sN,aes(x=item,y=ttr,color=factor(flash))) +
         geom_boxplot() +
         theme_bw() + xlab("Item") +
-        scale_color_discrete(name = "Test Administration", labels = c("Non-Flash", "Flash")) +
-        # scale_x_discrete(name="Item",breaks=colnames(noNA_sNL[,5:184]),limits=factor(sNLnames)) +
-        labs(title = paste("Flash differences in SPCPTN90 Speed")) +
-        theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+        scale_color_discrete(name = "Test Administration", labels = c("Non-Flash (n=133)", "Flash (n=145)")) +
+        labs(title = paste("Flash differences in SPCPTN90 Speed")) 
+
+ggsave("SPCPTN90_box_flash.png",height = 10,width = 15)
+
+
+
+
+# plot of means with error bars
+spe1_means <- ggplot(sNL_meansd,aes(x=Item,y=MeanTTR,color=factor(Flash))) +
+        geom_point(position=position_dodge(.9)) +
+        geom_errorbar(aes(ymin=MeanTTR-sd, ymax=MeanTTR+sd), width=.2,position=position_dodge(.9)) +
+        theme_bw() + ylab("Response Time (ms)") +
+        scale_color_discrete(name = "Test Administration", labels = c("Non-Flash (n=1511)", "Flash (n=20475)")) +
+        labs(title = paste("Flash differences in SPCPTNL Speed"))
+
+ggsave("SPCPTNL_means.png",height = 10,width = 30)
+
+
+spe2_means <- ggplot(sN_meansd,aes(x=Item,y=MeanTTR,color=factor(Flash))) +
+        geom_point(position=position_dodge(.9)) +
+        geom_errorbar(aes(ymin=MeanTTR-sd, ymax=MeanTTR+sd), width=.2,position=position_dodge(.9)) +
+        theme_bw() + ylab("Response Time (ms)") +
+        scale_color_discrete(name = "Test Administration", labels = c("Non-Flash (n=133)", "Flash (n=145)")) +
+        labs(title = paste("Flash differences in SPCPTN90 Speed"))
+
+ggsave("SPCPTN90_means.png",height = 10,width = 15)
+
+
+
+
+
+# log transform plots
+
+spe_log_box1 <- ggplot(sNL,aes(x=item,y=log(ttr+1),color=factor(flash))) +
+        geom_boxplot() +
+        theme_bw() + xlab("Item") + ylab("Log Transform of Response Time (ms)") +
+        scale_color_discrete(name = "Test Administration", labels = c("Non-Flash (n=1511)", "Flash (n=20475)")) +
+        labs(title = paste("Flash differences in SPCPTNL Speed (log transformed)"))
+
+ggsave("SPCPTNL_box_log.png",height = 10,width = 30)
+
+no_out_log_box1 <- ggplot(sNL,aes(x=item,y=log(ttr+1),color=factor(flash))) +
+        geom_boxplot(outlier.shape = NA) +
+        theme_bw() + xlab("Item") + ylab("Log Transform of Response Time (ms)") +
+        scale_color_discrete(name = "Test Administration", labels = c("Non-Flash (n=1511)", "Flash (n=20475)")) +
+        labs(title = paste("Flash differences in SPCPTNL Speed (log transformed)"))
+
+ggsave("SPCPTNL_box_log_nooutliers.png",height = 10,width = 30)
+
+
+spe_log_box2 <- ggplot(sN,aes(x=item,y=log(ttr+1),color=factor(flash))) +
+        geom_boxplot() +
+        theme_bw() + xlab("Item") + ylab("Log Transform of Response Time (ms)") +
+        scale_color_discrete(name = "Test Administration", labels = c("Non-Flash (n=1511)", "Flash (n=20475)")) +
+        labs(title = paste("Flash differences in SPCPTN90 Speed (log transformed)"))
+
+ggsave("SPCPTN90_box_log.png",height = 10,width = 15)
+
+no_out_log_box2 <- ggplot(sN,aes(x=item,y=log(ttr+1),color=factor(flash))) +
+        geom_boxplot(outlier.shape = NA) +
+        theme_bw() + xlab("Item") + ylab("Log Transform of Response Time (ms)") +
+        scale_color_discrete(name = "Test Administration", labels = c("Non-Flash (n=1511)", "Flash (n=20475)")) +
+        labs(title = paste("Flash differences in SPCPTN90 Speed (log transformed)"))
+
+ggsave("SPCPTN90_box_log_nooutliers.png",height = 10,width = 15)
+
+
+
+
+
+noNA_sNL[,5:184] <- sapply(noNA_sNL[,5:184]+1,log)
+noNA_sN[,5:94] <- sapply(noNA_sN[,5:94]+1,log)
+
+sNL_fs <- noNA_sNL[noNA_sNL$flash ==1,grepl("TTR",colnames(noNA_sNL))]  # noNA_sNL flash group
+sNL_ns <- noNA_sNL[noNA_sNL$flash ==0,grepl("TTR",colnames(noNA_sNL))]  # noNA_sNL non-flash group
+sN_fs <- noNA_sN[noNA_sN$flash ==1,grepl("TTR",colnames(noNA_sN))]  # noNA_sN flash group
+sN_ns <- noNA_sN[noNA_sN$flash ==0,grepl("TTR",colnames(noNA_sN))]  # noNA_sN non-flash group
+
+
+sNL_fmeans <- data.frame(colMeans(sNL_fs,na.rm = T))
+sNL_nmeans <- data.frame(colMeans(sNL_ns,na.rm = T))
+sN_fmeans <- data.frame(colMeans(sN_fs,na.rm = T))
+sN_nmeans <- data.frame(colMeans(sN_ns,na.rm = T))
+
+sNL_fsd <- data.frame(apply(sNL_fs, 2, sd,na.rm=T))
+sNL_nsd <- data.frame(apply(sNL_ns, 2, sd,na.rm=T))
+sN_fsd <- data.frame(apply(sN_fs, 2, sd,na.rm=T))
+sN_nsd <- data.frame(apply(sN_ns, 2, sd,na.rm=T))
+
+sNL_fmeansd <- cbind(rownames_to_column(sNL_fmeans),sNL_fsd)
+names(sNL_fmeansd) <- c("Item","MeanTTR","sd")
+sNL_fmeansd$Flash <- 1
+sNL_nmeansd <- cbind(rownames_to_column(sNL_nmeans),sNL_nsd)
+names(sNL_nmeansd) <- c("Item","MeanTTR","sd")
+sNL_nmeansd$Flash <- 0
+sN_fmeansd <- cbind(rownames_to_column(sN_fmeans),sN_fsd)
+names(sN_fmeansd) <- c("Item","MeanTTR","sd")
+sN_fmeansd$Flash <- 1
+sN_nmeansd <- cbind(rownames_to_column(sN_nmeans),sN_nsd)
+names(sN_nmeansd) <- c("Item","MeanTTR","sd")
+sN_nmeansd$Flash <- 0
+
+sNL_meansd <- rbind(sNL_fmeansd,sNL_nmeansd)
+sN_meansd <- rbind(sN_fmeansd,sN_nmeansd)
+
+
+log_spe1_means <- ggplot(sNL_meansd,aes(x=Item,y=MeanTTR,color=factor(Flash))) +
+        geom_point(position=position_dodge(.9)) +
+        geom_errorbar(aes(ymin=MeanTTR-sd, ymax=MeanTTR+sd), width=.2,position=position_dodge(.9)) +
+        theme_bw() + ylab("Log transformed Response Time (ms)") +
+        scale_color_discrete(name = "Test Administration", labels = c("Non-Flash (n=1511)", "Flash (n=20475)")) +
+        labs(title = paste("Flash differences in SPCPTNL Speed (log transformed)"))
+
+ggsave("SPCPTNL_logmeans.png",height = 10,width = 30)
+
+
+spe2_means <- ggplot(sN_meansd,aes(x=Item,y=MeanTTR,color=factor(Flash))) +
+        geom_point(position=position_dodge(.9)) +
+        geom_errorbar(aes(ymin=MeanTTR-sd, ymax=MeanTTR+sd), width=.2,position=position_dodge(.9)) +
+        theme_bw() + ylab("Log transformed Response Time (ms)") +
+        scale_color_discrete(name = "Test Administration", labels = c("Non-Flash (n=133)", "Flash (n=145)")) +
+        labs(title = paste("Flash differences in SPCPTN90 Speed (Log transformed)"))
+
+ggsave("SPCPTN90_logmeans.png",height = 10,width = 15)
+
 
 
 
